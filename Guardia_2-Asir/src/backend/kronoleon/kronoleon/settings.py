570fc03eb155c972b750bdde7cf6b3778ebdcef1
Alejandro Meléndez Fernández <alexmelfer.amf@gmail.com>
2025-12-13 23:00:18 +0100

    Base de datos conectada al backend

--- Guardia_2-Asir/src/backend/kronoleon/kronoleon/settings.py
+++ Guardia_2-Asir/src/backend/kronoleon/kronoleon/settings.py
@@ -74,20 +74,14 @@ WSGI_APPLICATION = 'kronoleon.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'guardiasdb',      
-#         'USER': 'alex_django',              
-#         'PASSWORD': 'ale123',       
-#         'HOST': '10.255.255.254',      
-#         'PORT': '3306',
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'GUARDIASDB',      
+        'USER': 'dbadmin',              
+        'PASSWORD': '#kr0n0L30N*',       
+        'HOST': '62.175.162.13',      
+        'PORT': '3306',
     }
 }
 
