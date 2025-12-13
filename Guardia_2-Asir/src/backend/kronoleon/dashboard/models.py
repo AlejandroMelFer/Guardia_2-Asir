a2709bdb80f23bc9550cf625fc975afd16c61f2e
MikeSrz <onriosrz@gmail.com>
2025-12-13 22:56:00 +0100

    Pequeña modificacion de typo

--- Guardia_2-Asir/src/backend/kronoleon/dashboard/models.py
+++ Guardia_2-Asir/src/backend/kronoleon/dashboard/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 
-from django.db import models
-
 class Profesor(models.Model):
     id_profesor = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=45)
