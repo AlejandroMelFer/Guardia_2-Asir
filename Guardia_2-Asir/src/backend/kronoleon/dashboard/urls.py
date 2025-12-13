b3fa74d5d08a65863fc6e6f04ca739c989b06e74
MikeSrz <onriosrz@gmail.com>
2025-12-13 22:49:03 +0100

    Añadiendo modulo de actualizacion de datos de usuario

--- Guardia_2-Asir/src/backend/kronoleon/dashboard/urls.py
+++ Guardia_2-Asir/src/backend/kronoleon/dashboard/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('home/', views.home_view, name='home'),
+    path('dashboard/', views.dashboard_view, name='dashboard'),
 ]
--- Guardia_2-Asir/src/backend/kronoleon/dashboard/views.py
+++ Guardia_2-Asir/src/backend/kronoleon/dashboard/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .models import Profesor, Clase
 
 @login_required
 def home_view(request):
     return render(request, 'dashboard/home.html')
+
+@login_required
+def dashboard_view(request):
+    try:
+        # Asumimos que el email del usuario coincide con el del Profesor
+        profesor = Profesor.objects.get(email=request.user.email)
+        datos = Clase.objects.filter(profesor=profesor).select_related('asignatura', 'aula', 'hora')
+    except Profesor.DoesNotExist:
+        datos = []
+    
+    return render(request, 'dashboard/dashboard.html', {'datos': datos})
